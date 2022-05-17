373d4d4f28959487301bdef6b24c638673aa1969
Kyutae Kim <kqt99@naver.com>
2022-05-17 15:38:02 +0900

    err

--- day0512/src/King.java
+++ day0512/src/King.java
@@ -1,4 +1,15 @@
 
 public class King {
+	private String name;
+	private int order;
+	
+	King(String name, int order) {
+		this.name = name;
+		this.order = order;
+	}
+	
+	void show() {
+		System.out.println("Á¶¼±" + order + "´ë¿Õ " + name);
+	}
 
 }
--- day0512/src/Main.java
+++ day0512/src/Main.java
@@ -1,9 +1,52 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("[ menu ]");
+		System.out.println("0 : exit");
+		System.out.println("1 : input (King Info.}");
+		System.out.println("2 : show  (King List)");
+		System.out.println("3 : menu");
+		
+		King [] k = new King[30];
+		Scanner sc = new Scanner(System.in);
+		int mode = 4; int cnt = 0;
+		
+		while(mode != 0) {
+			System.out.print(">");
+			mode = sc.nextInt();
+			switch(mode) {
+				case 0:
+					System.out.println("종료되었습니다");
+					break;
+				case 1:
+					System.out.print("왕의 이름 > ");
+					String name = sc.next();
+					System.out.print("왕의 순서 > ");
+					int order = sc.nextInt();
+					k[cnt++] = new King(name, order);
+					break;
+					
+				case 2:
+					for(int i=0; i<cnt; i++) {
+						k[i].show();
+					}
+					break;
+					
+				case 3:
+					System.out.println("[ menu ]");
+					System.out.println("0 : exit");
+					System.out.println("1 : input (King Info.");
+					System.out.println("2 : show  (King List)");
+					System.out.println("3 : menu");
+					break;
+					
+			}
+			
+		}
+		
 	}
 
 }
